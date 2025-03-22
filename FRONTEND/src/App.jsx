3aed45dda85502524c716846645d46758fdc7d6c
Felipe Cordeiro <felipecordeirocruz@gmail.com>
2025-03-21 22:24:47 -0300

    feat: add routes for pages login, register and dashboard

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,12 +1,21 @@
-import Header from "./components/Header"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-
-  return (
-    <>
-    <Header />
-    </>
-  )
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/dashboard" element={<Layout />}>
+                    <Route index element={<Dashboard />} />
+                </Route>
+            </Routes>
+        </Router>
+    );
 }
 
-export default App
+export default App;
